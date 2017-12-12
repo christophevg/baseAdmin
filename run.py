11a4b9307bafed182f769db0c9153d0774e6bd3f
Christophe VG <contact@christophe.vg>
2017-12-13 00:54:20 +0100

    load environment variables from env.local

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), "env.local")
+load_dotenv(dotenv_path)
+
 from backend import server
 
 server.run(debug=True, host="0.0.0.0")
