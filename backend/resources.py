8605cf8cbf300e0b43ee09837c526bce06a5e7c9
Christophe VG <contact@christophe.vg>
2017-12-13 00:54:51 +0100

    provide access to server-side environment vars

--- backend/resources.py
+++ backend/resources.py
@@ -1,3 +1,5 @@
+import os
+
 from flask_restful import Resource
 
 from backend import app, api, mongo, authenticate
@@ -10,4 +12,11 @@ class Connection(Resource):
       'mongo': str(mongo.db),
     }
 
-api.add_resource(Connection, '/api/status')
+api.add_resource(Connection, "/api/status")
+
+class Environment(Resource):
+  @authenticate(["admin"])
+  def get(self, variable):
+    return os.environ.get(variable)
+
+api.add_resource(Environment, "/api/env/<string:variable>")
