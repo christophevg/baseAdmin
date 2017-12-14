b6d6bf60b26e31fe422da302c2cfa0f20f05cae9
Christophe VG <contact@christophe.vg>
2017-12-14 13:05:28 +0100

    refactored MQ connection setup and tracking

--- backend/mq.py
+++ backend/mq.py
@@ -5,6 +5,7 @@ import paho.mqtt.client as mqtt
 
 from urllib.parse import urlparse
 
+from backend      import HOSTNAME, IP
 from backend.web  import server
 from backend.data import store
 
@@ -44,24 +45,29 @@ def on_message(client, userdata, msg):
   for callback in subscriptions[msg.topic]:
     callback(str(msg.payload.decode("utf-8")))
 
-mq = mqtt.Client()
-mq.on_connect = on_connect
-mq.on_message = on_message
-
-logging.debug("connecting to MQ " + MQ_HOSTNAME + ":" + str(MQ_PORT))
-mq.connect(MQ_HOSTNAME, MQ_PORT)
-mq.loop_start()
-
 def track_client_status(msg):
-  # clientId:[online|offline]
   (client, status) = str(msg).split(":")
   with server.app_context():
-    print(client, status)
     store.db.clients.update_one(
       { "_id": client },
       { "$set" : { "status" : status } },
       upsert=True
     )
 
-logging.debug("following client/status")
-follow("client/status", track_client_status)
+def track_clients():
+  logging.debug("following client/status")
+  follow("client/status", track_client_status)
+
+mq = None
+CLIENT_ID = HOSTNAME + "@" + IP
+
+def connect(clientId=CLIENT_ID):
+  clientId = clientId + "@" + HOSTNAME + "(" + IP + ")"
+  mq = mqtt.Client()
+  mq.on_connect = on_connect
+  mq.on_message = on_message
+  mq.will_set("client/status", clientId + ":offline", 0, False)
+  logging.debug("connecting to MQ " + MQ_HOSTNAME + ":" + str(MQ_PORT))
+  mq.connect(MQ_HOSTNAME, MQ_PORT)
+  mq.loop_start()
+  mq.publish("client/status",  clientId + ":online",  0, False)
--- run.py
+++ run.py
@@ -2,4 +2,6 @@ import backend.config
 
 from backend.web import server
 
+backend.mq.track_clients()
+backend.mq.connect("backend")
 server.run(debug=True, host="0.0.0.0")
