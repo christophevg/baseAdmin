35fe8c50b45864d2b3a41d635746b538e79a17a3
Christophe VG <contact@christophe.vg>
2017-12-14 21:41:08 +0100

    client fetches MQ connection details from backend

--- backend/client.py
+++ backend/client.py
@@ -1,9 +1,46 @@
+import sys
+import argparse
+import requests
+import logging
+
 import backend
-from backend import HOSTNAME, IP
+import backend.logging
 
 class Client():
-  def __init__(self, name="client"):
+  def __init__(self, name="client", description=None):
     self.name = name
+    if description is None:
+      description = self.name + ": a baseAdmin client."
+
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("--backend",  type=str, help="backend url.",      default=None)
+    parser.add_argument("--username", type=str, help="backend user.",     default=None)
+    parser.add_argument("--password", type=str, help="backend password.", default=None)
+
+    args = parser.parse_args()
+
+    self.backend  = args.backend
+    self.username = args.username
+    self.password = args.password
+    self.mq       = None
 
   def run(self):
-    backend.mq.connect(self.name)
+    if not self.get_mq_connection_details(): return False
+    backend.mq.connect(self.name, mq=self.mq)
+    return True
+    
+  def get_mq_connection_details(self):
+    if not self.backend: return True
+
+    logging.info("requesting MQ connection details from " + self.backend)
+    response = requests.get(
+      self.backend + "/api/mq/connection/mqtt",
+      auth=(self.username, self.password)
+    )
+
+    if response.status_code != requests.codes.ok:
+      logging.error("request for MQ connection details failed: " + str(response.status_code))
+      return False
+
+    self.mq = response.json()
+    return True
--- console.py
+++ console.py
@@ -6,11 +6,10 @@ import backend.config
 from backend.client import Client
 
 console = Client("console")
-console.run()
-
-try:
-  logging.info("starting console event loop, interrupt with Ctrl+c")
-  while True:
+if console.run():
+  try:
+    logging.info("starting console event loop, interrupt with Ctrl+c")
+    while True:
+      pass
+  except KeyboardInterrupt:
     pass
-except KeyboardInterrupt:
-  pass
