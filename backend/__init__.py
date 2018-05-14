6446ddc16ff5c2c9c69f50ec09147012e76ac08a
Christophe VG <contact@christophe.vg>
2018-05-14 13:05:57 +0200

    moved logging config to backend for shared use

--- backend/__init__.py
+++ backend/__init__.py
@@ -12,6 +12,24 @@ s.connect(("8.8.8.8", 80))
 IP = s.getsockname()[0]
 s.close()
 
+# change logging formatter
+
+import logging
+
+formatter = logging.Formatter(
+  "%(asctime)s [%(levelname)-5.5s] %(message)s"
+)
+logger = logging.getLogger()
+
+logger.setLevel(logging.DEBUG) # TODO make this configurable
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(formatter)
+logger.addHandler(consoleHandler)
+
+def disable_console_logging():
+  logger.removeHandler(consoleHandler)
+
 # ensure we have local environment in scope, also from dot env file
 
 import logging
@@ -25,7 +43,6 @@ dotenv_path = join(script_path, "env.local")
 if isfile(dotenv_path):
   logging.info("loading local environment configuration from " + dotenv_path)
   load_dotenv(dotenv_path)
-
 
 import backend.web
 import backend.rest
--- client/__init__.py
+++ client/__init__.py
@@ -1,19 +1,5 @@
-# change logging formatter
-
-import logging
-
-formatter = logging.Formatter(
-  "%(asctime)s [%(levelname)-5.5s] %(message)s"
-)
-logger = logging.getLogger()
-
-logger.setLevel(logging.DEBUG) # TODO make this configurable
-
-consoleHandler = logging.StreamHandler()
-consoleHandler.setFormatter(formatter)
-logger.addHandler(consoleHandler)
-
 import os
+import logging
 
 LOGFILE = os.environ.get("LOGFILE")
 if LOGFILE:
@@ -21,9 +7,6 @@ if LOGFILE:
   fileHandler = logging.FileHandler(LOGFILE)
   fileHandler.setFormatter(formatter)
   logger.addHandler(fileHandler)
-
-def disable_console_logging():
-  logger.removeHandler(consoleHandler)
 
 # silence loggers of some libs
 
