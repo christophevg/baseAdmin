a6e44d6afb75264f6d2b6fb0710d0b7321a02668
Christophe VG <contact@christophe.vg>
2018-06-18 21:45:15 +0200

    fixed regression in mongo URI handling

--- backend/store/__init__.py
+++ backend/store/__init__.py
@@ -7,11 +7,13 @@ from backend import MASTER, APP_NAME
 
 MONGO_URI = os.environ.get("MONGODB_URI")
 if not MONGO_URI:
-  MONGO_URI = "mongodb://localhost:27017/";
+  MONGO_URI = "mongodb://localhost:27017/" + ("store" if MASTER else "cloud")
 
 logging.debug("connecting to " + MONGO_URI)
 mongo = MongoClient(MONGO_URI)
 
-store = mongo.store if MASTER else mongo.cloud
+database = MONGO_URI.split("/")[-1]
+
+store = mongo[database]
 
 import backend.provision
