56cc5ca0c7e31d8042ffc5978fe9e750e27c0698
Christophe VG <contact@christophe.vg>
2018-06-18 20:49:06 +0200

    reintroduced backend.web for Heroku deploy ;-)

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,21 +1,6 @@
 import os
 import logging
 
-import backend.web
-import backend.rest
-import backend.interface
-
-from backend import MASTER
-
-if MASTER:
-  import backend.resources.master
-  logging.info("Running Backend in MASTER mode")
-  from backend.plugins.master import *
-else:
-  import backend.resources.cloud
-  logging.info("Running Backend in CLOUD mode")
-  from backend.plugins.cloud import *
-
 from backend.web import server
 
 DEBUG = os.environ.get("BACKEND_DEBUG")
--- backend/web.py
+++ backend/web.py
@@ -1,3 +1,5 @@
+import logging
+
 from flask import Flask
 import jinja2
 
@@ -8,3 +10,17 @@ my_loader = jinja2.ChoiceLoader([
   server.jinja_loader,
 ])
 server.jinja_loader = my_loader
+
+import backend.rest
+import backend.interface
+
+from backend import MASTER
+
+if MASTER:
+  import backend.resources.master
+  logging.info("Running Backend in MASTER mode")
+  from backend.plugins.master import *
+else:
+  import backend.resources.cloud
+  logging.info("Running Backend in CLOUD mode")
+  from backend.plugins.cloud import *
