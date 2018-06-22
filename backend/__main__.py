c6cd92f5da6d3ab0e413162cc5e0f582039006db
Christophe VG <contact@christophe.vg>
2018-06-22 12:26:52 +0200

    added support for running backend on SSL

--- backend/__main__.py
+++ backend/__main__.py
@@ -10,4 +10,11 @@ PORT = os.environ.get("BACKEND_PORT")
 if not PORT:
   port = 5000
 
-server.run(debug=DEBUG, host="0.0.0.0", port=PORT)
+SSL = os.environ.get("SSL") in ["Yes", "yes", "Y", "y", "True", "true"]
+if SSL:
+  CERT = os.environ.get("SSL_CERT") or "etc/cert.pem"
+  KEY  = os.environ.get("SSL_KEY")  or "etc/key.pem"
+  context = (CERT, PEM)
+  server.run(host='0.0.0.0', port=PORT, ssl_context=context, threaded=True, debug=True)
+else:
+  server.run(debug=DEBUG, host="0.0.0.0", port=PORT)
