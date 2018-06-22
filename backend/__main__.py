e01274d801946c097b4254378fc580e52ae771e8
Christophe VG <contact@christophe.vg>
2018-06-22 12:29:22 +0200

    fixed typo

--- backend/__main__.py
+++ backend/__main__.py
@@ -14,7 +14,7 @@ SSL = os.environ.get("SSL") in ["Yes", "yes", "Y", "y", "True", "true"]
 if SSL:
   CERT = os.environ.get("SSL_CERT") or "etc/cert.pem"
   KEY  = os.environ.get("SSL_KEY")  or "etc/key.pem"
-  context = (CERT, PEM)
+  context = (CERT, KEY)
   server.run(host='0.0.0.0', port=PORT, ssl_context=context, threaded=True, debug=True)
 else:
   server.run(debug=DEBUG, host="0.0.0.0", port=PORT)
