00d2b791b7de2031e8770d0bd3f67bd14bcee791
Christophe VG <contact@christophe.vg>
2018-06-24 21:06:21 +0200

    added simple link to __default__ client's config

--- backend/static/master/setup.js
+++ backend/static/master/setup.js
@@ -4,11 +4,22 @@ var Setup = {
     <h1>Status</h1>
     <div v-if="status()" v-html="status()"></div>
     <div v-else>loading setup status...</div>
+    <h1>Default Client</h1>
+    <div>
+      Use the button below to create default settings for new clients...<br>
+      <v-btn @click="showDefaultClient(); return false;"
+             color="primary" class="white--text" round>
+        Default Configuration
+      </v-btn>
+    </div>
   </div>
 `,
   methods: {
     status : function() {
       return store.getters.setupStatus();
+    },
+    showDefaultClient: function() {
+      this.$router.push("/client/__default__");
     }
   },
   created: function() {
