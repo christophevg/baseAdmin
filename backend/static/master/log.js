8d421b7be87a1653785f3d029f74dfef39257866
Christophe VG <contact@christophe.vg>
2018-06-30 10:44:12 +0200

    logs regarding clients now link to client page

--- backend/static/master/log.js
+++ backend/static/master/log.js
@@ -1,18 +1,34 @@
 var Log = {
   template : `
-<v-layout justify-center column>
-  <v-card v-for="(message, i) in messages()" :key="i">
-    <v-card-title>
-      <div>
-        <span class="grey--text">{{ message.topic }}</span><br>
-        <span>{{ message.payload }}</span>
-      </div>
-    </v-card-title>
-  </v-card>
-</v-layout>`,
-  methods: {
+<div>
+  <h1>Logged Activity</h1>
+  <v-layout justify-center column>
+    <v-card v-for="(message, i) in messages" :key="i">
+      <v-card-title>
+        <div>
+          <v-btn v-if="message.client" depressed small color="primary" @click="gotoClient(message.client)">
+            {{ message.client }}
+          </v-btn>
+          <div v-else class="grey--text">{{ message.topic }}</div>
+          <span>{{ message.payload }}</span>
+        </div>
+      </v-card-title>
+    </v-card>
+  </v-layout>
+</div>`,
+  computed: {
     messages : function() {
-      return store.getters.messages();
+      var messages = store.getters.messages();
+      for(var i in messages) {
+        messages[i]["client"] =
+          messages[i].topic[0] == "client" ? messages[i].topic[1] : false;
+      }
+      return messages;
+    }
+  },
+  methods: {
+    gotoClient: function(client) {
+      this.$router.push("/client/" + client);
     }
   }
 };
