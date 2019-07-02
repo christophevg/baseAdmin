62b0ab6ac70e6e5410b0bf29c0042960e6395dbc
Christophe VG <contact@christophe.vg>
2019-07-02 22:41:53 +0200

    second 1.0.0 rc

--- tests/test_pki.py
+++
@@ -1,7 +0,0 @@
-from baseadmin import pki
-
-def test_provisioning(db):
-  assert db.list_collection_names() == []
-  pki.keys.provision()
-  assert db.list_collection_names() == [ "pki" ]
-  assert len([x for x in db.pki.find()]) == 1
--- tests/test_storage.py
+++
@@ -1,5 +0,0 @@
-def test_provisioning(db):
-  data = [ {"data" : 1 }, { "data" : 2 }, { "data" : 3 }]
-  db.provision("test", data)
-  assert db.list_collection_names() == [ "test" ]
-  assert [ x for x in db.test.find() ] == data
