a8d547bf88d76c6b88e459bfa793bab7daf22211
Christophe VG <contact@christophe.vg>
2019-07-02 23:02:42 +0200

    fixed bad test syntax

--- baseadmin/components/system/screen/update-screen-orientation.sh
+++ baseadmin/components/system/screen/update-screen-orientation.sh
@@ -14,25 +14,25 @@ SPLASH=/usr/share/plymouth/themes/pix/splash.png
 case "${ORIENTATION}" in
 "CW")
   sudo cp 99-fbturbo-cw.conf  ${XCONF}
-  if -f ~/splash-cw.png; then
+  if [ -f ~/splash-cw.png ]; then
     sudo cp ~/splash-cw.png   ${SPLASH}
   fi
   ;;
 "CCW")
   sudo cp 99-fbturbo-ccw.conf ${XCONF}
-  if -f ~/splash-ccw.png; then
+  if [ -f ~/splash-ccw.png ]; then
     sudo cp ~/splash-ccw.png   ${SPLASH}
   fi
   ;;
 "UD")
   sudo cp 99-fbturbo-ud.conf  ${XCONF}
-  if -f ~/splash-ud.png; then
+  if [ -f ~/splash-ud.png ]; then
     sudo cp ~/splash-ud.png   ${SPLASH}
   fi
   ;;
 *)
   sudo cp 99-fbturbo.conf     ${XCONF}
-  if -f ~/splash.png; then
+  if [ -f ~/splash.png ]; then
     sudo cp ~/splash.png   ${SPLASH}
   fi
   ;;
